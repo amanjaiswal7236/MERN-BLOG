3f9d1b07ac279eb46c74fb1bedbc1c86684c34a6
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-02-04 14:57:17 +0530

    sign up pages functionality added

--- api/index.js
+++ api/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-const app = express();
+import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
+const app = express();
+app.use(express.json());
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URI)
@@ -16,6 +19,20 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
+
+app.use((err,req,res,next)=>{
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success:false,
+    statusCode,
+    message
+  })
+
+})

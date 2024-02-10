3839c4f8b4fbdbcdba413d63f53df353d107b31b
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-02-11 01:30:35 +0530

    user post added

--- api/models/post.model.js
+++ api/models/post.model.js
@@ -25,6 +25,11 @@ const postSchema = new mongoose.Schema({
         default: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.hostinger.com%2Ftutorials%2Fhow-to-write-a-blog-post&psig=AOvVaw1Qu4yJR3tS6eePIMJkwKVW&ust=1707383969044000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMCroIrzmIQDFQAAAAAdAAAAABAE"
 
     },
+    category: {
+        type: String,
+        required: true,
+        default: "uncategorized"
+    },
     
 },{timestamps:true});
 

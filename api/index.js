9b9baf626175880b3117377edd6bb04c60272f87
AMAN JAISWAL <124071449+amanjaiswal7236@users.noreply.github.com>
2024-02-12 22:03:39 +0530

    users page and functionallity added

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
 import postRoutes from './routes/post.routes.js';
+import commentRoutes from './routes/comment.routes.js';
 
 import cookieParser from 'cookie-parser';
 
@@ -26,6 +27,7 @@ app.get('/', (req, res) => {
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
 app.use('/api/post',postRoutes);
+app.use('/api/comment',commentRoutes);
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,11 +11,14 @@ import PrivateRoute from './components/PrivateRoute'
 import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
 import CreatePost from './pages/CreatePost'
 import UpdatePost from './pages/UpdatePost'
+import PostPage from './pages/PostPage'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
 
   return (
     <BrowserRouter>
+    <ScrollToTop />
     <Header/>
       <Routes>
         <Route path="/" element={<Home/>} />
@@ -30,6 +33,8 @@ function App() {
           <Route path="/update-post/:postId" element={<UpdatePost/>} />
         </Route>
         <Route path="/projects" element={<Projects/>} />
+        <Route path="/post/:postSlug" element={<PostPage/>} />
+        
 
       </Routes>
     <Footer />
